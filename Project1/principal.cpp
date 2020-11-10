37a9349be64ce231d9c1a5e76247e8ce168398e3
brayan146 <74216517+brayan146@users.noreply.github.com>
2020-11-10 09:59:41 -0400

    Add files via upload

---
+++ Project1/principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Nombre:Brayan Zuñiga" << endl;
+	cout << "Carera:Ing. Sistemas" << endl;
+	cout << "Edad:18 años" << endl;
+	system("pause");
+	return 0;
+
+}
